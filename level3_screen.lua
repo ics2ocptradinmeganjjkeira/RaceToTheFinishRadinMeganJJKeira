7707a0b2edb10013ff4b4b4303f4c8cc0da51eb4
John Omage <john.junior.omage@ocsbstudent.ca>
2019-05-28 08:41:01 -0400

    change

---
+++ level3_screen.lua
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- Created by: Megan.S
+-- Date: Month Day, Year
+-- Description: This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+
+
+composer.gotoScene( "level1_screen" )
