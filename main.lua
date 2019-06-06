96ae59733320c8abb4e454f70dc566b88a47c92d
Megan Souannhaphanh <47784122+s256420@users.noreply.github.com>
2019-06-06 09:44:46 -0400

    Changed

--- main.lua
+++ main.lua
@@ -17,6 +17,6 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "main_menu" )
+composer.gotoScene( "splash_screen" )
 
 
